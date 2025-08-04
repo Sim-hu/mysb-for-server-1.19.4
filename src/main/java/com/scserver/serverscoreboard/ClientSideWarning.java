980c542da621ef19bb7eaa37b41c777fc5fe2275
Sim-hu <93906619+Sim-hu@users.noreply.github.com>
2025-08-04 23:33:32 +0900

    Create ClientSideWarning.java

---
+++ src/main/java/com/scserver/serverscoreboard/ClientSideWarning.java
@@ -0,0 +1,13 @@
+package com.scserver.serverscoreboard;
+
+/**
+ * このmodはサーバー側専用です。
+ * クライアント側に導入しても何も効果がありません。
+ * 
+ * This mod is server-side only.
+ * Installing on client side will have no effect.
+ */
+public class ClientSideWarning {
+    // このクラスは警告用のコメントのみを含みます
+    // This class contains only warning comments
+}
